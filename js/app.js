1b7c7947aaadf60c62c9a1e6dab7976a229222ae
alainmelendez <alainjeshua@gmail.com>
2023-04-12 21:01:56 -0300

    Aplicacion Presupuesto con JS, cambios en Index y App

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const Ingresos = [
+    new Ingreso('Salario', 2100.00),
+    new Ingreso('Venta Coche', 1500)
+];
+
+const Egresos = [
+    new Egreso('Renta Departamento', 900),
+    new Egreso('Ropa', 400)
+];
+
+let cargarApp = ()=>{
+    cargarCabecero();
+}
+
+let cargarCabecero = ()=>{
+    
+}
