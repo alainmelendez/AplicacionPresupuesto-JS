ede2f73f8d136ffcfc7b6492e491bb80bf5749a3
alainmelendez <alainjeshua@gmail.com>
2023-04-24 15:40:12 -0300

    Listados Egresos con JavaScript

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const ingresos = [
     new Ingreso('Salario', 2100.00),
-    new Ingreso('Venta Coche', 1500)
+    new Ingreso('Venta Coche', 1500),
+    
 ];
 
 const egresos = [
@@ -10,6 +11,7 @@ const egresos = [
 
 let cargarApp = ()=>{
     cargarCabecero();
+    cargarIngresos();
 }
 
 let totalIngresos = ()=>{
@@ -32,7 +34,7 @@ let cargarCabecero = ()=>{
     let presupuesto = totalIngresos() - totalEgresos();
     let porcentajeEgreso = totalEgresos() / totalIngresos();
     document.getElementById('presupuesto').innerHTML = formatoMoneda(presupuesto);
-    document.getElementById('porcentaje').innerHTML = formatoporcentaje(porcentajeEgreso);
+    document.getElementById('porcentaje').innerHTML = formatoPorcentaje(porcentajeEgreso);
     document.getElementById('ingresos').innerHTML = formatoMoneda(totalIngresos());
     document.getElementById('egresos').innerHTML = formatoMoneda(totalEgresos());
 }
@@ -45,3 +47,29 @@ const formatoPorcentaje = (valor) =>{
     return valor.toLocaleString('en-US', {style:'percent', minimumFractionDigits:2})
 }
 
+const cargarIngresos = ()=>{
+    let ingresosHTML = '';
+    for(let ingreso of ingresos){
+
+        ingresosHTML += crearIngresoHTML(ingreso);
+    }
+    document.getElementById('lista-ingresos').innerHTML = ingresosHTML;
+}
+
+const crearIngresoHTML = (ingreso)=>{
+let ingresoHTML = `
+    <div class="elemento limpiarEstilos">
+    <div class="elemento_descricion">${ingreso.descripcion}</div>
+    <div class="derecha limpiarEstilos">
+        <div class="elemento_valor">${formatoMoneda(ingreso.valor)}</div>
+        <div class="elemento_eliminar">
+            <button class='elemento_eliminar--btn'>
+                <ion-icon name='close-circle-outline'></ion-icon>
+            </button>
+        </div>
+    </div>
+</div>
+    `;
+    return ingresoHTML;
+
+}
